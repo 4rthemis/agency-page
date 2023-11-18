f8ae15357cbcf388447a053f1e45070c0aec0498
4rthemis <syahrul.uix@gmail.com>
2023-11-19 00:53:27 +0700

    fix

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -7,28 +7,37 @@ import "../About/About.css";
 const AboutComponent = () => {
   return (
     <section className="py-10" id="About">
-      <div className="about-section container mx-auto flex flex-col items-center justify-center px-4">
-        <div className="mb-8">
-          <img
-            data-aos="fade-right"
-            data-aos-duration="1000"
-            src={AboutImage}
-            alt="About Us"
-            className="w-full h-auto max-w-full rounded-lg"
-          />
+      <div className="about-section container mx-auto flex flex-col md:flex-row items-center justify-between px-4">
+        <div className="md:w-1/2 md:ml-0 ml-4">
+          <div className="relative max-w-full overflow-hidden">
+            <img
+              data-aos="fade-down"
+              data-aos-duration="800"
+              src={Decoration}
+              alt="Decoration"
+              className="absolute right-0 top-0 w-80 hidden md:block"
+            />
+            <img
+              data-aos="fade-right"
+              data-aos-duration="1000"
+              src={AboutImage}
+              alt="About Us"
+              className="w-full h-auto max-w-full rounded-lg pt-24"
+            />
+          </div>
         </div>
-        <div className="text-center">
+        <div className="md:w-1/2 mt-4 md:mt-0">
           <h2
             data-aos="fade-down"
             data-aos-duration="800"
-            className="text-3xl font-semibold mb-4"
+            className="text-3xl font-semibold mb-10 sm:justify-items-center"
           >
             About Us
           </h2>
           <p
             data-aos="fade-left"
             data-aos-duration="800"
-            className="text-gray-700 mb-4"
+            className="text-gray-700 justify-content-center"
           >
             Zara Creative Agency is a digital agency from Jakarta. We believe in
             pushing boundaries, thinking beyond the ordinary, and transforming
@@ -38,7 +47,7 @@ const AboutComponent = () => {
           <button
             data-aos="zoom-in"
             data-aos-duration="1000"
-            className="hover:bg-blue-900 text-white py-2 px-4 rounded capitalize"
+            className=" hover:bg-blue-900 text-white py-2 px-4 rounded mt-10 capitalize"
           >
             Read More
           </button>
